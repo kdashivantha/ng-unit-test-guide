5c99dfb5b3b9389d21d4f944a67a6c56eda3b5e9
Amith Shivantha <kdashivantha@gmail.com>
2020-01-10 16:34:58 +0530

    Spies are an easy way to check if a function was called or to provide a custom return value

--- src/app/load-http-data/data-load.service.spec.ts
+++ src/app/load-http-data/data-load.service.spec.ts
@@ -1,7 +1,7 @@
 import { TestBed, async, fakeAsync, tick } from "@angular/core/testing";
 import { HttpClient, HttpClientModule } from "@angular/common/http";
 import { of } from "rxjs";
-import { delay } from 'rxjs/internal/operators';
+import { delay } from "rxjs/internal/operators";
 import { DataLoadService } from "./data-load.service";
 
 let httpClientSpy: { get: jasmine.Spy };
@@ -20,7 +20,7 @@ describe("HttpDataLoadService", () => {
     }).compileComponents();
   }));
 
-  it("should give us application information :  Promise", (done) => {
+  it("should give us application information :  Promise", done => {
     var service = TestBed.get(DataLoadService) as DataLoadService;
 
     service
@@ -37,7 +37,7 @@ describe("HttpDataLoadService", () => {
       });
   });
 
-  it("should give us application information - async : Promise", async() => {
+  it("should give us application information - async : Promise", async () => {
     var service = TestBed.get(DataLoadService) as DataLoadService;
 
     await service.Load();
@@ -58,15 +58,23 @@ describe("HttpDataLoadService", () => {
     expect(data.Version).toBe(2);
   }));
 
-  it("should give us application information - Done : Observables", (done) => {
+  it("should give us application information - Done : Observables", done => {
     var service = TestBed.get(DataLoadService) as DataLoadService;
 
-    service.LoadObs().subscribe(v => { 
-        let data = service.data;
-        expect(data.AppName).toBe("My APP");
-        expect(data.Version).toBe(2);
-        done(); 
+    service.LoadObs().subscribe(v => {
+      let data = service.data;
+      expect(data.AppName).toBe("My APP");
+      expect(data.Version).toBe(2);
+      done();
     });
   });
 
+  it("Spy can check a fucntion is called-through", () => {
+    var service = TestBed.get(DataLoadService) as DataLoadService;
+    const spyOnMethod = spyOn(service, "LoadObs").and.callThrough();
+    // act
+    service.LoadObs().subscribe();
+    // assert
+    expect(spyOnMethod).toHaveBeenCalled();
+  });
 });
